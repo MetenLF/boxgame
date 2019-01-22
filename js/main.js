43030e738153472f6a49fb2b0c689407ef5b54f5
Andrei Watanabe <andrei@suthub.com>
2019-01-22 00:30:48 -0200

    Added milestones, function to eval milestones, reworked worker hire func

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 /*------------------------------------------------------------------------------
-Plans (old):
-
-PRIORITY: make some testing utils
-
-- add a way to make money off boxes
-- add a hint thing on the top like in infinity clicker
-- end of the world trigger when there's too much pox
-- only cats survive
-- "meowconomy" - after all who do you think that keeps buying boxes
-- warp space developments: how to make boxes bigger on the inside
-- warp space attacts CHAOS.
-- initiate the DARK MILLENIUM protocol for war 
-
 Plans (new!):
 OK- rewrite this entire crap in vanilla javascript
 
@@ -19,27 +6,38 @@ OK- fucking start a repo
 
 OK- rework game data variables into a huge object so I can work better later on the save and load
 
-- make game autoload when page loads
+OK- make game autoload when page loads
 
-- make sure interface is automatically updated with that (logic rework)
+OK- make sure interface is automatically updated with that (logic rework)
 
-- save game and load game
+OK- save game and load game
 . save game
 . load game
 . delete save
 . buttons to do these things
 
+OK- Implement milestones as a new and more versatile way to display new interface elements
+
+TECHNICALLY OK WILL BE DEVELOPED LATER- Make an achievements object
+
+OK- make the hire box maker function a more versatile and global worker hire function
+
+- Finally implement box baker and the baked boxes
+
 - finish game's core features:
 . counters
 . workers
 . values
-. save/load
 . testing utilities
 . upgrades
 . some progression
-. an alert box on the top or something
+. an alert box on the top or something to substitute window alerts
 
-- finally go use some fucking framework
+- rewrite everything using more modern javascript or typescript I have no clue
+
+- use something else for the interface and learn some css preprocessor
+
+- finally go use some fucking framework like goddamn react or vue or angular
 ------------------------------------------------------------------------------*/
 
 // START GLOBAL VARS -----------------------------------------------------------
@@ -48,36 +46,65 @@ OK- rework game data variables into a huge object so I can work better later on 
 Data structure should be splitting stuff into what is owned by what.
 */
 var gameData = {
-    'counters': {
-        'numSmallBoxes': 0,
-        'numBakedBoxes': 0,
-        'numPoxWaked': 0
+  'counters': {
+    'numSmallBoxes': 0,
+    'numBakedBoxes': 0,
+    'numPoxWaked': 0
+  },
+  'workers': {
+    'boxMakers': {
+      'amount': 0,
+      'cost': 10,
+      'costIncrement': 1.1
     },
-    'workers': {
-        'boxMakers': {
-            'amount': 0,
-            'cost': 10,
-            'costIncrement': 1.1
-        },
-        'boxBakers': {
-            'amount': 0,
-            'cost': 25,
-            'costIncrement': 1.3
-        },
-        'poxWakers': {
-            'amount': 0,
-            'cost': 100,
-            'costIncrement': 1.6
-        }
+    'boxBakers': {
+      'amount': 0,
+      'cost': 25,
+      'costIncrement': 1.3
     },
-    'milestones': {
-
-    },
-    'etc': {
-        'doNot': 0,
-        'moreEvilDoNot': 0
-
+    'poxWakers': {
+      'amount': 0,
+      'cost': 100,
+      'costIncrement': 1.6
     }
+  },
+  'milestones': [
+    {
+      'name': 'boxCounter',
+      'triggered': false,
+      'condition': {
+        'resource': 'gameData.counters.numSmallBoxes',
+        'comparison': '>=',
+        'threshold': 1
+      } 
+    },
+    {
+      'name': 'boxFactory',
+      'triggered': false,
+      'condition': {
+        'resource': 'gameData.counters.numSmallBoxes',
+        'comparison': '>=',
+        'threshold': 10
+      }
+    },
+    {
+      'name': 'smallBoxMaker',
+      'triggered': false,
+      'condition': {
+        'resource': 'gameData.workers.boxMakers.amount',
+        'comparison': '>=',
+        'threshold': 1
+      }
+    }
+  ],
+  'achievements': {
+
+  },
+  'etc': {
+    'doNot': 0,
+    'moreEvilDoNot': 0
+
+  }
 }
 
 
@@ -101,15 +128,15 @@ function makeSmallBox() {
   document.getElementById('number-small-boxes').innerHTML = ++gameData.counters.numSmallBoxes;
 }
 
-// hire small box maker button
-function hireBoxMaker() {
-    ++gameData.workers.boxMakers.amount;
+// hire worker button
+function hireWorker(workerType) {
+    ++gameData['workers'][workerType]['amount'];
 
     // Deduct cost
-    gameData.counters.numSmallBoxes -= gameData.workers.boxMakers.cost;
+    gameData['counters']['numSmallBoxes'] -= gameData['workers'][workerType]['cost'];
 
     // Increase cost for the next one, using Math.ceil() to round up
-    gameData.workers.boxMakers.cost = Math.ceil(gameData.workers.boxMakers.cost * gameData.workers.boxMakers.costIncrement);
+    gameData['workers'][workerType]['cost'] = Math.ceil(gameData['workers'][workerType]['cost'] * gameData['workers'][workerType]['costIncrement']);
 }
 
 // do not click thing
@@ -211,27 +238,51 @@ function interfaceIO() {
 function interfaceDisplayer() {
 
   // show counter if any boxes were made
-  if (gameData.counters.numSmallBoxes >= 1) {
+  if (gameData.milestones[0].triggered) {
     showNewElement('statistics-card');
     showNewElement('counter-small-boxes');
   }
 
   // show factory if we have at least 10 boxes
-  if (gameData.counters.numSmallBoxes >= 10) {
+  if (gameData.milestones[1].triggered) {
     showNewElement('box-factory');
   }
 
-  if (gameData.workers.boxMakers.amount >= 1) {
+  // show box makers counter if we have at least 1 box maker
+  if (gameData.milestones[2].triggered) {
     showNewElement('counter-box-makers');
-  }
-
-  // example fucky milestone
-  if (gameData.counters.numSmallBoxes >= 9000) {
-    // don't forget to think about this
   }
     
 }
 
+/*
+this exists so when a player reaches 9001 boxes and buys an upgrade worth 9000 
+boxes and then saves the game, when they reload it, the interface of the
+unlocked element will disappear, making it very confusing.
+
+This crazy function is a way to work around that!
+*/
+function milestoneTriggerer() {
+  for (var index = 0; index < gameData.milestones.length; index++) {
+
+    if (!gameData.milestones[index].triggered) {
+      
+      var interpretedCondition = [
+        'if (',
+        gameData.milestones[index].condition.resource,
+        gameData.milestones[index].condition.comparison,
+        gameData.milestones[index].condition.threshold,
+        '){',
+        'gameData.milestones[index].triggered = true;',
+        '}'
+      ];
+
+      eval(interpretedCondition.join(''));
+    }
+    
+  }
+
+}
 
 // END INTERVAL MANAGERS -------------------------------------------------------
 
@@ -242,7 +293,8 @@ window.setInterval(function () {
   counterUpdater();
   priceUpdater();
   interfaceIO();
-  interfaceDisplayer()
+  interfaceDisplayer();
+  milestoneTriggerer();
 }, 10);
 
 // END INTERVAL FUNCTION -------------------------------------------------------
